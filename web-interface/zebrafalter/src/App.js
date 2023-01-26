652008b64e760865e9c996daf295a589ed67bb04
McCain <mbcarruthers@crimson.ua.edu>
2023-01-26 03:15:41 -0500

    added better styling for navigation bar

--- web-interface/zebrafalter/src/App.js
+++ web-interface/zebrafalter/src/App.js
@@ -11,14 +11,15 @@ const App = () => {
       <BrowserRouter>
           <div className="app">
               <header className="p-3 bg-dark text-white container-fluid text-center sticky-top">
-                  <div className="container">
-                      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start text-center">
-                          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                              <li><Link to="/" className="nav-link px-2 change-color no-decor btn btn-outline-light m-2">Home</Link></li>
-                              <li><Link to="/observations" className="nav-link px-2 change-color no-decor btn btn-outline-light m-2">Monarch Map</Link></li>
-                          </ul>
-                      </div>
-                  </div>
+                  <nav className="nav-row navbar">
+                      <Link to="/" className="no-decor mx-1 px-1 nav-link">
+                          <h4 className="nav-text">Home</h4>
+                      </Link>
+                      <Link to="/observations" className="no-decor mx-1 px-1 nav-link">
+                          <h4 className="nav-text">Monarch Map</h4>
+                      </Link>
+
+                  </nav>
               </header>
 
               <div className="local">
--- web-interface/zebrafalter/src/components/Landing.jsx
+++ web-interface/zebrafalter/src/components/Landing.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../style/Landing.css";
 
-// 49766 48662
+// Monarch and ZebraLongwing taxon_id's 49766 48662
 
 const Landing = () => {
     return(
         <div className="container-fluid text-center outline">
-            <h1>Landing</h1>
+            <h1 className="comfort">Landing</h1>
             <div className="container-fluid outline">
                 <img src={"\/\/\localhost:8025/btrfly.jpg"} alt="not working" className="btrfly-image m-2"/>
             </div>
